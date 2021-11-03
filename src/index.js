a0720eda7bf22e205c36fd17bd4503125e40a3e0
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-11-03 16:24:15 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+
+  // grab form add submit listener initiate build function, prevent default adn reset input
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let person = form["new_task_person"].value;
+    let todo = form["new-task-description"].value;
+    let color = form["selectPriority"].value;
+    buildToDo(person, todo, color);
+    form.reset();
+  });
 });
+
+function buildToDo(person, todo, color) {
+  // set variables,elements,selectors
+  let newListItem = document.createElement("li");
+  newListItem.className = "list-item";
+  let btn = document.createElement("button");
+
+  // assign values
+  btn.textContent = "✔";
+  newListItem.textContent = `${person}: ${todo}`;
+  newListItem.style.color = color;
+
+  // appending to
+  newListItem.appendChild(btn);
+  document.querySelector("#tasks").appendChild(newListItem);
+
+  // add eventListener
+  btn.addEventListener("click", handleDelete);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
